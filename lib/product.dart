79dec2f2e92cb74f45b364988d791cd7a53a21c5
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-02 23:49:32 +0330

    Create Product Entity

---
+++ lib/product.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Product {
+  int id;
+  String imagePach;
+
+  String title;
+  String price;
+  Product({
+    required this.id,
+    required this.imagePach,
+    required this.title,
+    required this.price,
+  });
+}
