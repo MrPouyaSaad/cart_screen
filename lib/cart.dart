2cdf1e31a168c6ebcd06858f2a651500ebce87bb
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-02 23:46:08 +0330

    App Bar

--- lib/cart.dart
+++ lib/cart.dart
@@ -10,6 +10,21 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Column(
+          children: [
+            const Text('My Cart'),
+            Text(
+              '3 Items',
+              style: themeData.textTheme.bodySmall,
+            ),
+          ],
+        ),
+        leading: const BackButton(),
+      ),
+    );
   }
 }
